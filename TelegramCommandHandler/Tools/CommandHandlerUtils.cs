aec2ad1d59e06bb5e15a27faa63a83412d6e7b23
Ali Bdeir <ali@bdeir.org>
2020-03-06 13:40:38 +0200

    Major Refactoring

---
+++ TelegramCommandHandler/Tools/CommandHandlerUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TelegramCommandHandler.Tools
+{
+    public static class CommandHandlerUtils
+    {
+
+        public static bool IsSameOrSubclass(Type potentialBase, Type potentialDescendant)
+        {
+            return potentialDescendant.IsSubclassOf(potentialBase)
+                   || potentialDescendant == potentialBase;
+        }
+
+        public static bool IsAsync(this MethodInfo m)
+        {
+            return m?
+                .GetCustomAttribute<AsyncStateMachineAttribute>()?
+                .StateMachineType?
+                .GetTypeInfo()
+                .GetCustomAttribute<CompilerGeneratedAttribute>()
+                != null;
+        }
+
+
+    }
+}
