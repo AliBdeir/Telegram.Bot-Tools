7967e95058bd4809458b47f0510556cbfa207e3d
Ali Bdeir <developer.ali.bdeir@gmail.com>
2020-03-26 21:52:46 +0200

    Interactivity WIP

---
+++ Interactivity/Models/InteractivityObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Telegram.Bot.Types;
+
+namespace Interactivity.Models
+{
+
+    public interface IInteractivityObject { }
+
+    public class InteractivityObject<T> : IInteractivityObject
+    {
+
+        public TimeSpan TimeSpan { get; set; } = TimeSpan.FromMinutes(2);
+        public Chat Chat { get; set; }
+        public Predicate<T> Predicate { get; set; }
+        public EventWaitHandle WaitHandle { get; set; } = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+        public InteractivityObject(Chat chat, Predicate<T> predicate)
+        {
+            this.Chat = chat;
+            this.Predicate = predicate;
+        }
+
+    }
+}
