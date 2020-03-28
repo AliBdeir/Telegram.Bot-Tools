64c4c5b30c69bc34e6a34dc51a47e18eb5c435c9
Ali Bdeir <developer.ali.bdeir@gmail.com>
2020-03-28 14:24:58 +0200

    Initial Commit

---
+++ Interactivity/Extensions/TelegramBotClientExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Interactivity.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Interactivity.Extensions
+{
+    public static class TelegramBotClientExtensions
+    {
+
+        private static TelegramInteractivity currentInteractivity;
+
+        public static void UseInteractivity(this TelegramBotClient client, TelegramInteractivity interactivity)
+        {
+            currentInteractivity = interactivity;
+        }
+
+        public static TelegramInteractivity GetInteractivity(this TelegramBotClient client)
+        {
+            return currentInteractivity;
+        }
+
+        public static async Task<InteractivityResult<Message>> WaitForMessageAsync(
+            this TelegramBotClient client,
+            Predicate<Message> predicate,
+            TimeSpan? defaultTimeSpanOverride,
+            )
+
+    }
+}
