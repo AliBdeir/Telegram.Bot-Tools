b47da40a87ab886eaf2e283d23fde423e3bfc0c5
Ali Bdeir <ali@bdeir.org>
2020-03-28 14:24:58 +0200

    Initial Commit

---
+++ Interactivity/Models/InteractivityResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interactivity.Models
+{
+    public class InteractivityResult<T>
+    {
+        public T Result { get; set; }
+        public bool TimedOut { get; set; }
+    }
+}
